b61bfa53d8ae26fe931256f4ff394ca8c18918f3
gitzart <windowxpxp@gmail.com>
2017-04-27 00:15:36 +0630

    Redesign the map as objects

--- app/js/googlemap.js
+++ app/js/googlemap.js
@@ -3,155 +3,289 @@ define([
 ], function(mapapi) {
   'use strict';
 
-  var GoogleMap = function() {
+  /* =======================================================================
+   =================================== Marker
+   ========================================================================= */
+  /**
+   * A marker to pinpoint a position on the map.
+   *
+   * @class
+   * @param {google.maps.Map} map - The map to set the marker on.
+   */
+  function Marker(map) {
     var self = this;
 
-    // Initialize necessary map features
-    self._map = new google.maps.Map(document.querySelector('#map-canvas'));
-    self._bounds = undefined;
-    self._markers = [];
-    self._infowindows = [];
+    self._map = map;
+    self._marker = undefined;
 
-    // Create the map
-    self.create = function(opts) {
-      self._map.setOptions(opts);
+    self.event = {
+      click: function(cb) {
+        self._marker.addListener('click', cb);
+        return self.event;
+      },
+      mouseOver: function(cb) {
+        self._marker.addListener('mouseover', cb);
+        return self.event;
+      },
+      mouseOut: function(cb) {
+        self._marker.addListener('mouseout', cb);
+        return self.event;
+      },
     };
 
-    // Create a rectangle area with geographical coordinates
-    // Return a bound object
-    self.createBound = function(latLng) {
-      if (latLng) {
-        return new google.maps.LatLngBounds(latLng.sw, latLng.ne);
-      } else {
-        return new google.maps.LatLngBounds();
-      }
+    self.trigger = {
+      click: function() {
+        google.maps.event.trigger(self._marker, 'click');
+      },
+      mouseOver: function() {
+        google.maps.event.trigger(self._marker, 'mouseover');
+      },
+      mouseOut: function() {
+        google.maps.event.trigger(self._marker, 'mouseout');
+      },
     };
+  }
 
-    // Adjust the map size to see all the markers
-    self.fitBounds = function(bounds) {
-      self._map.fitBounds(bounds || self._bounds);
-    };
-
-    // Create a marker
-    // Return a marker object
-    self.addMarker = function(opts) {
-      return new google.maps.Marker(opts);
-    };
-
-    // Show the marker on the map
-    self.showMarker = function(marker) {
-      if (!marker.getMap()) {
-        marker.setMap(self._map);
-      }
-    };
-
-    // Hide the marker and marker related features (i.e, infowindow)
-    self.hideMarker = function(marker) {
-      if (marker.getMap()) {
-        self.triggerInfoWindowEvent(
-          self._infowindows[marker.get('id')],
-          'closeclick'
-        );
-        marker.setMap(null);
-      }
-    };
-
-    // Create an array of markers and bound them to the map
-    // Return a marker array
-    self.addMarkers = function(data) {
-      self._bounds = self.createBound();
-      data.forEach(function(v, i) {
-        var marker = self.addMarker({
-          map: self._map,
-          id: i,
-          position: v.position,
-          title: v.title,
-          animation: google.maps.Animation.DROP,
-        });
-        self._bounds.extend(marker.getPosition());
-        self._markers.push(marker);
-        self.setMarkerEvents(marker);
-      });
-      self.fitBounds();
-      return self._markers;
-    };
-
-    // Animate the marker to bounce for the given time
-    // Default to 2 times
-    self.bounceMarker = function(marker, time) {
-      var durationPerBounce = 700;
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function() {
-        marker.setAnimation(null);
-      }, time ? time * durationPerBounce : 2 * durationPerBounce);
-    };
-
-    // Create an infowindow
-    // Return an infowindow object
-    self.addInfoWindow = function(opts) {
-      return new google.maps.InfoWindow(opts);
-    };
-
-    // Attach an infowindow to a marker
-    self.openInfoWindow = function(infowindow, marker) {
-      infowindow.open(self._map, marker);
-    };
-
-    // Add event listeners to the marker
-    self.setMarkerEvents = function(marker) {
-      // On mouse over, change the marker `icon`
-      marker.addListener('mouseover', function() {
-        marker.setIcon({
-          url: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-          scaledSize: new google.maps.Size(40, 40),
-        });
-      });
-
-      // On mouse out, set the default marker `icon`
-      marker.addListener('mouseout', function() {
-        marker.setIcon(null);
-      });
-
-      // On click, animate the marker, create an infowindow and
-      // open it on the marker
-      marker.addListener('click', function() {
-        self.bounceMarker(marker);
-        var index = marker.get('id');
-        var infowindow = self._infowindows[index];
-        if (!infowindow) {
-          infowindow = self.addInfoWindow({
-            id: index,
-            content: marker.getTitle(),
-          });
-          self._infowindows[index] = infowindow;
-          self.setInfoWindowEvents(infowindow);
-        }
-        self.openInfoWindow(infowindow, marker);
-      });
-    };
-
-    // Add event listeners to the infowinow
-    self.setInfoWindowEvents = function(infowindow) {
-      // On close button click, remove the infowindow from the DOM
-      infowindow.addListener('closeclick', function() {
-        infowindow.close();
-      });
-    };
-
-    // Trigger the marker events
-    self.triggerMarkerEvent = function(marker, event) {
-      if (marker && event) {
-        google.maps.event.trigger(marker, event);
-      }
-    };
-
-    // Trigger the infowindow events
-    self.triggerInfoWindowEvent = function(infowindow, event) {
-      if (infowindow && event) {
-        google.maps.event.trigger(infowindow, event);
-      }
-    };
+  /**
+   * Create a marker.
+   *
+   * @param {?Object} [opts] - The options to create a marker with.
+   * @see {@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#MarkerOptions}
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.create = function(opts) {
+    this._marker = new google.maps.Marker(opts || null);
+    this._marker.setMap(this._map);
+    if (opts && opts.animation) {
+      this._marker.setAnimation(google.maps.Animation.DROP);
+    }
+    return this;
   };
+
+  /**
+   * Set the visibility of the marker.
+   *
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.show = function() {
+    this._marker.setVisible(true);
+    return this;
+  };
+
+  /**
+   * Remove the visibility of the marker.
+   *
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.hide = function() {
+    this._marker.setVisible(false);
+    return this;
+  };
+
+  /**
+   * Change the default marker icon.
+   *
+   * @param {string} [icon=https://maps.google.com/mapfiles/ms/icons/blue-dot.png] - The marker icon URL.
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.addIcon = function(icon) {
+    icon = icon || 'https://maps.google.com/mapfiles/ms/icons/blue-dot.png';
+    this._marker.setIcon({
+      url: icon,
+      scaledSize: new google.maps.Size(40, 40),
+    });
+    return this;
+  };
+
+  /**
+   * Set the default marker icon.
+   *
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.removeIcon = function() {
+    this._marker.setIcon(null);
+    return this;
+  };
+
+  /**
+   * Animate the marker with a bounce effect.
+   *
+   * @param {number} [time=2] - The number of time the marker bounces.
+   * @return {Marker} The current `Marker` instance.
+   */
+  Marker.prototype.bounce = function(time) {
+    var DURATION_PER_BOUNCE = 700;
+    time = time ? DURATION_PER_BOUNCE * time : DURATION_PER_BOUNCE * 2;
+    this._marker.setAnimation(google.maps.Animation.BOUNCE);
+    setTimeout((function() {
+      this._marker.setAnimation(null);
+    }).bind(this), time);
+    return this;
+  };
+
+  /* =======================================================================
+   =================================== Bounds
+   ========================================================================= */
+  /**
+   * A latlng bound object to set the map viewport to contain markers.
+   *
+   * @class
+   * @param {google.maps.Map} map - The map to bound the markers on.
+   */
+  function Bounds(map) {
+    this._map = map;
+    this._bounds = undefined;
+  }
+
+  /**
+   * Create a [google.maps.LatLngBounds]{@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#LatLngBounds} instance.
+   *
+   * @param {?Object} [sw] - The southwest latitude and longitude.
+   * @param {?Object} [ne] - The northeast latitude and longitude.
+   * @return {Bounds} The current `Bounds` instance.
+   */
+  Bounds.prototype.create = function(sw, ne) {
+    this._bounds = new google.maps.LatLngBounds(sw || null, ne || null);
+    return this;
+  };
+
+  /**
+   * Extend the bounding area.
+   *
+   * @param {Object} latlng - The latitude and longitude.
+   * @return {Bounds} The current `Bounds` instance.
+   */
+  Bounds.prototype.extend = function(latlng) {
+    this._bounds.extend(latlng);
+    return this;
+  };
+
+  /**
+   * Contain the markers inside the map viewport.
+   *
+   * @return {Bounds} The current `Bounds` instance.
+   */
+  Bounds.prototype.fit = function() {
+    this._map.fitBounds(this._bounds);
+    return this;
+  };
+
+  /* =======================================================================
+   =================================== InfoWindow
+   ========================================================================= */
+  /**
+   * An information window for a specific position.
+   *
+   * @class
+   * @param {google.maps.Map} map - The map to display the info windows on.
+   */
+  function InfoWindow(map) {
+    var self = this;
+
+    self._map = map;
+    self._infoWindow = undefined;
+
+    self.event = {
+      close: function(cb) {
+        self._infoWindow.addListener('closeclick', cb);
+        return self.event;
+      },
+    };
+
+    self.trigger = {
+      close: function() {
+        google.maps.event.trigger(self._infoWindow, 'closeclick');
+      },
+    };
+  }
+
+  /**
+   * Create an infoWindow.
+   *
+   * @param {?Object} [opts] - The options to create an infoWindow with.
+   * @see {@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#InfoWindowOptions}
+   * @return {InfoWindow} The current `InfoWindow` instance.
+   */
+  InfoWindow.prototype.create = function(opts) {
+    this._infoWindow = new google.maps.InfoWindow(opts || null);
+    return this;
+  };
+
+  /**
+   * Open the infoWindow on the map.
+   *
+   * @param {*} anchor - The anchor to attach the infoWindow to. Consult the [`open` method of `InfoWindow` class]{@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#InfoWindow}.
+   * @return {InfoWindow} The current `InfoWindow` instance.
+   */
+  InfoWindow.prototype.open = function(anchor) {
+    this._infoWindow.open(this._map, anchor || null);
+    return this;
+  };
+
+  /**
+   * Close the infoWindow.
+   *
+   * @return {InfoWindow} The current `InfoWindow` instance.
+   */
+  InfoWindow.prototype.close = function() {
+    this._infoWindow.close();
+    return this;
+  };
+
+  /* =======================================================================
+   =================================== GoogleMap
+   ========================================================================= */
+  /**
+   * A [Google Map]{@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#Map} instance.
+   *
+   * @class
+   */
+  function GoogleMap() {
+    var self = this;
+
+    self._map = undefined;
+    self._center = undefined;
+
+    /**
+     * Create a map.
+     *
+     * @param {Object} container - The DOM element to place a map in.
+     * @param {?Object} [opts] - The options to create a map with.
+     * @see {@link https://developers.google.com/maps/documentation/javascript/3.exp/reference#MapOptions}
+     * @return {GoogleMap} The current `GoogleMap` instance.
+     */
+    self.create = function(container, opts) {
+      self._map = new google.maps.Map(container, opts || null);
+      return self;
+    };
+
+    /**
+     * A [Marker]{@link Marker} object.
+     *
+     * @return {Marker} The new `Marker` instance.
+     */
+    self.marker = function() {
+      return new Marker(self._map);
+    };
+
+    /**
+     * A [Bounds]{@link Bounds} object.
+     *
+     * @return {Bounds} The new `Bounds` instance.
+     */
+    self.bounds = function() {
+      return new Bounds(self._map);
+    };
+
+    /**
+     * An [InfoWindow]{@link InfoWindow} object.
+     *
+     * @return {InfoWindow} The new `InfoWindow` instance.
+     */
+    self.infoWindow = function() {
+      return new InfoWindow(self._map);
+    };
+  }
 
   return GoogleMap;
 });
