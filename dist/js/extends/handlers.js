a74f239dac9f10e33ddc6928adf59926c249e507
gitzart <windowxpxp@gmail.com>
2017-04-30 17:44:29 +0630

    Add final production version and documentation

---
+++ dist/js/extends/handlers.js
@@ -0,0 +1,2 @@
+define(["jquery","knockout"],function(e,n){"use strict";n.bindingHandlers.fadeVisible={update:function(n,i){return i()?e(n).fadeIn(200):e(n).fadeOut(200)}}});
+//# sourceMappingURL=handlers.js.map
---
+++ dist/js/models/Location.js
@@ -0,0 +1,2 @@
+define(["knockout"],function(i){"use strict";function e(e,n,o,r,t,s,l){var u=this;u.id=e,u.name=n,u.position=o,u.marker=r||null,u.infoWindow=t||null,u.isFavorite=i.observable(s||!1),u.isFiltered=i.observable(l||!0)}return e});
+//# sourceMappingURL=Location.js.map
